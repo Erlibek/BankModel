43c91b366ce90b4723a25a3c23fd4e1e69ac658d
Erlibek <shaikemelov99@gmail.com>
2022-03-30 23:52:30 +0600

    abstract bank task

---
+++ src/Account.java
@@ -0,0 +1,13 @@
+public abstract class Account {
+    private String name;
+    private int amount;
+
+    public void pay(int amount) {
+    }
+
+    public void transfer(Account account, int amount) {
+    }
+
+    public void addMoney(int amount) {
+    }
+}
